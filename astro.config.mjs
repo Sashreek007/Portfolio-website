242705060d574eb7dd95986894c4341ed941110c
Sashreek007 <Sashreek.addanki@gmail.com>
2026-02-06 13:44:20 -0700

    made vercel deployment better

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [],
 });
